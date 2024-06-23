c8c6cfd6aed77aecee40d374add6c6d09f3779f7
divyavinod6 <102822213+divyavinod6@users.noreply.github.com>
2024-06-23 15:11:48 +0000

    updated font

--- code/main.ts
+++ code/main.ts
@@ -1,19 +1,19 @@
 import kaboom from "kaboom"
 import "kaboom/global"
 
-document.title='Coder, Coffee and Bugs'
+document.title = 'Coder, Coffee and Bugs'
 
 // initialize context
 kaboom({
-  font: "sink",
-  background: [210, 210, 155,]
-})
+  font: "sans",
+  background: [210, 210, 155],
+});
 
 // load sprites
 loadSprite("bug", "sprites/bug.png")
 loadSprite("coffee", "sprites/coffeeMug.png")
 loadSprite("player", "sprites/player.png")
-loadSprite("background","sprites/backgroundImage.jpg")
+loadSprite("background", "sprites/backgroundImage.jpg")
 
 // load music
 loadSound("score", "sounds/score.mp3")
@@ -34,9 +34,9 @@ let gameOverText;
 
 
 // load high score from local storage
-  if (localStorage.getItem("highScore")) {
-    HIGH_SCORE = parseInt(localStorage.getItem("highScoreText") || "0", 10);
-  }
+if (localStorage.getItem("highScore")) {
+  HIGH_SCORE = parseInt(localStorage.getItem("highScoreText") || "0", 10);
+}
 
 
 // display score
@@ -46,8 +46,8 @@ const displayScore = () => {
   }
   // score counter
   scoreText = add([
-    text("SCORE: " + SCORE,{
-      font:"sans"
+    text("SCORE: " + SCORE, {
+      font: "sans"
     }),
     scale(1),
     pos(width() - 181, 48),
@@ -61,8 +61,8 @@ const displayHighScore = () => {
     destroy(highScoreText);
   }
   highScoreText = add([
-    text("HIGH SCORE: " + HIGH_SCORE,{
-      font:"serif"
+    text("HIGH SCORE: " + HIGH_SCORE, {
+      font: "sans"
     }),
     scale(1),
     pos(width() - 270, 21),
@@ -80,43 +80,43 @@ const playBg = () => {
 }
 
 // diplay game over
-const displayGameOver =()=>{
-  if(gameOverText){
+const displayGameOver = () => {
+  if (gameOverText) {
     destroy(gameOverText)
   }
-  gameOverText=add([
-    text("GAME OVER",{
-      font:"serif",
-      size:48,
+  gameOverText = add([
+    text("GAME OVER", {
+      font: "sans",
+      size: 48,
     }),
     scale(3),
-    pos(10,21),
-    color(10,10,255)    
+    pos(10, 21),
+    color(10, 10, 255)
   ]);
 }
 // Reset function
-const resetGame=()=>{
-  if(gameOverText){
+const resetGame = () => {
+  if (gameOverText) {
     destroy(gameOverText);
   }
   // update high score if current score is higher
-  if(SCORE > HIGH_SCORE){
-    HIGH_SCORE=SCORE;
+  if (SCORE > HIGH_SCORE) {
+    HIGH_SCORE = SCORE;
     localStorage.setItem("highscore", HIGH_SCORE.toString());
   }
   // reset game variables
   SCORE = 0;
-  BSPEED=2;
+  BSPEED = 2;
 
   // destroy all existing game elements
   destroyAll("bug");
   destroyAll("coffee");
-  
+
   // recreate player
-  if(player){
+  if (player) {
     destroy(player);
   }
-  
+
   player = add([
     sprite("player"),  // renders as a sprite
     pos(120, 80),    // position in world
@@ -124,54 +124,19 @@ const resetGame=()=>{
     scale(0.13)
   ]);
 
-  
+
   // Display initial score and high score
   displayScore();
-  displayHighScore();  
+  displayHighScore();
 
   // re register collision handlers after reset
   registerCollisionHandlers();
 };
 
-// add backgound image
-add([
-  sprite("background", {width:width(),height:height()}),
-  pos(0,0),
-  z(-1)
-])
-
-// Add player
-player = add([
-  sprite("player"),  // renders as a sprite
-  pos(120, 80),    // position in world
-  area(),          // has a collider
-  scale(0.13),
-])
-
-// Add events to player
-onKeyDown("left", () => {
-  playBg()
-  player.move(-SPEED, 0)
-})
-
-onKeyDown("right", () => {
-  playBg()
-  player.move(SPEED, 0)
-})
-
-onKeyDown("up", () => {
-  playBg()
-  player.move(0, -SPEED)
-})
-
-onKeyDown("down", () => {
-  playBg()
-  player.move(0, SPEED)
-})
 
 
 // function to continuously spawn bugs and coffee
-const spawnObejcts=() => {
+const spawnObejcts = () => {
   for (let i = 0; i < 4; i++) {
     let x = rand(0, width());
     let y = height()
@@ -205,12 +170,12 @@ const spawnObejcts=() => {
   if (BSPEED < 13) {
     BSPEED += 1;
   }
-  
+
 };
 
 
 // player collision with bug
-const registerCollisionHandlers = ()=>{
+const registerCollisionHandlers = () => {
   player.onCollide("bug", (b) => {
     if (backgroundMusic) {
       backgroundMusic.stop()
@@ -219,45 +184,72 @@ const registerCollisionHandlers = ()=>{
     addKaboom(player.pos);
     // diplay game over text
     displayGameOver();
-    // gameOverText=add([
-    //   text("Game Over",{
-    //     font:"serif"
-    //   }),
-    //   scale(3),
-    //   pos(10,21),
-    //   color(10,10,255)
-    // ]);
-    // reset game after short delay
-    wait(2,()=>{
+    wait(2, () => {
       resetGame();
-      if(backgroundMusic){  // change
+      if (backgroundMusic) {  // change
         backgroundMusic.play()
       }
     });
   });
   player.onCollide("coffee", (coffee) => {
-  
+
     play("sip", {
       volume: 3
     })
     destroy(coffee);
     SCORE += 1
-  
+
     displayScore();
-  
-  
+
+
   });
 }
 
 // continuosuly spawn objects
-const spawnLoop=()=>{
-  loop(4,()=>{
+const spawnLoop = () => {
+  loop(4, () => {
     spawnObejcts();
   });
 };
 
 
-// initialise function to set up game
+// initialise functions to set up game
+// add backgound image
+add([
+  sprite("background", { width: width(), height: height() }),
+  pos(0, 0),
+  z(-1)
+])
+
+// Add player
+player = add([
+  sprite("player"),  // renders as a sprite
+  pos(120, 80),    // position in world
+  area(),          // has a collider
+  scale(0.13),
+])
+
+// Add events to player
+onKeyDown("left", () => {
+  playBg()
+  player.move(-SPEED, 0)
+})
+
+onKeyDown("right", () => {
+  playBg()
+  player.move(SPEED, 0)
+})
+
+onKeyDown("up", () => {
+  playBg()
+  player.move(0, -SPEED)
+})
+
+onKeyDown("down", () => {
+  playBg()
+  player.move(0, SPEED)
+})
+
 displayScore();
 displayHighScore();
 spawnLoop();
